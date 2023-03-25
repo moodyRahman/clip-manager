d8d5de04de484f8a80ecf7b0db31d49cc6503aa6
Moody Rahman <moody.byte@gmail.com>
2023-03-25 09:43:35 -0400

    enabled home module

--- backend/routes/resources/index.js
+++ backend/routes/resources/index.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import { clipsModule } from './clips.js'
-
+import { homeModule } from "./home.js"
 const router = express.Router()
 
 router.use("/clips", clipsModule)
+router.use("/home", homeModule)
 
 export { router as resourceModule }
