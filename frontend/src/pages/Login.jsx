1bab80ac8876a46086c195af5ac9c8331ac21946
Moody Rahman <moody.byte@gmail.com>
2023-03-25 15:36:22 -0400

    skeletal login logic

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,26 @@
+import { useRef, useState } from "react"
 
 
 const Login = () => {
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = () => {
+        console.log(username, password)
+    }
+
+    const handleChange = (setter) => {
+        return (e) => {
+            setter(e.target.value)
+        }
+    }
+
     return (
         <>
-            <input type="text" placeholder="username" />
+            <input onChange={handleChange(setUsername)} type="text" placeholder="username" value={username} />
+            <input onChange={handleChange(setPassword)} type="password" placeholder="password" value={password} />
+            <button onClick={handleSubmit}>submit</button>
         </>
     )
 }
