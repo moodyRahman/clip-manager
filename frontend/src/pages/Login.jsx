ad190ad4f9b2f9467dd05175f5f214332a5da86e
Moody Rahman <moody.byte@gmail.com>
2023-03-25 21:00:21 -0400

    starting login flow

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,22 @@
-import { useRef, useState } from "react"
-
+import { useEffect, useRef, useState } from "react"
+import * as AWS from 'aws-sdk/global';
 
 const Login = () => {
 
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
 
+    // tearing this straight out of the documentation
     const handleSubmit = () => {
-        console.log(username, password)
+        console.log(username, password, email)
+        var poolData = {
+            UserPoolId: `${import.meta.env.VITE_COGNITO_USER_POOL_ID}`, // Your user pool id here
+            ClientId: `${import.meta.env.VITE_COGNITO_CLIENT_ID}`, // Your client id here
+        };
+
+        var userPool = new CognitoUserPool(poolData);
+        var attributeList = [];
+
     }
 
     const handleChange = (setter) => {
