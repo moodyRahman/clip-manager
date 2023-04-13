d64bead7a7537aeef72ffd3ce12d9ec1f916e918
WenBin Lin <wblin2986@gmail.com>
2023-04-13 17:07:27 -0400

    wrong code

--- backend/routes/resources/clips.js
+++ backend/routes/resources/clips.js
@@ -1,12 +1,30 @@
 import express from "express";
 import AWS from "aws-sdk";
 import multer from "multer";
+import MongoDb from "mongodb";
 
 const upload = multer();
 const router = express.Router();
 const s3 = new AWS.S3();
+const MongoClient = MongoDb.MongoClient;
 
 const bucketName = "clip-manager-video-clips";
+
+//Create a MongoDB client, open a connection to DocDB; as a replica set,
+//  and specify the read preference as secondary preferred
+
+MongoClient.connect(
+	"mongodb://clipdbmanager:Aadocdb1243@docdb-clip-manager.cinjbaykl6cc.us-east-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=<path to rds-combined-ca-bundle.pem>&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false",
+
+	function (err, client) {
+		if (err) throw err;
+
+		//Specify the database to be used
+		console.log("Connected successfully to server");
+
+		client.close();
+	}
+);
 
 router.get("/", (req, res, next) => {
 	res.send("clips module online");
