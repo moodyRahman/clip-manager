006b9992ca66728293c9ba122a5922c536863b35
Moody Rahman <moody.byte@gmail.com>
2023-04-30 23:23:19 -0400

    enforce auth home text color

--- frontend/src/pages/AuthHome.jsx
+++ frontend/src/pages/AuthHome.jsx
@@ -1,15 +1,20 @@
 import { useSelector } from "react-redux";
+import styled from "styled-components";
 
 const AuthHome = () => {
 	const username = useSelector((state) => state.auth.username);
-    const userId = useSelector((state) => state.auth.userID);
+	const userId = useSelector((state) => state.auth.userID);
+
+	const Display = styled.div`
+		color:white;
+	`
 
 	return (
-		<>
+		<Display>
 			{typeof username === "string"
 				? `welcome ${username}! it's a pleasure to see you`
 				: null}
-		</>
+		</Display>
 	);
 };
 
